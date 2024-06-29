baf33312b54a4ede212deaa4089d36cbe2f9d726
Ignacio2100 <ignaciodeleone8@gmail.com>
2024-06-29 00:58:41 -0600

    diseño de login

---
+++ Proyecto/Models/UsuarioCLS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class UsuarioCLS
+    {
+        public int iidusuario { get; set; }
+
+        [Required]
+        public string nombreusuario { get; set; }
+
+        [Required]
+        public string contra { get; set; }
+
+        public string iidtipousuario { get; set; }
+        [Required]
+        public int iid { get; set; }
+
+    }
+}
