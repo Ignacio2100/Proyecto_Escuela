ddf52be719e30374446e85e030ee78a78ac2fa90
Ignacio2100 <ignaciodeleone8@gmail.com>
2024-08-04 23:29:05 -0600

    Parte 1 de animales

---
+++ Proyecto/Models/AnimalCLS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class AnimalCLS
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string TipoAnimal { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
