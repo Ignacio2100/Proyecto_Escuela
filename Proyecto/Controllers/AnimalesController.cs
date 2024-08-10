6b066c296fd2fba6408819d27879d9b1e40bbe8d
Ignacio2100 <ignaciodeleone8@gmail.com>
2024-08-09 20:44:49 -0600

    apartado de los animales

--- Proyecto/Controllers/AnimalesController.cs
+++ Proyecto/Controllers/AnimalesController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Proyecto.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,135 @@ namespace Proyecto.Controllers
 {
     public class AnimalesController : Controller
     {
+        public class AnimalesViewModel
+        {
+            public List<AnimalCLS> ListaMamiferos { get; set; }
+            public List<AnimalCLS> ListaAves { get; set; }
+            public List<AnimalCLS> ListaReptiles { get; set; }
+            public List<AnimalCLS> ListaAnfibios { get; set; }
+            public List<AnimalCLS> ListaVertebrados { get; set; }
+            public List<AnimalCLS> ListaInvertebrados { get; set; }
+            public AnimalCLS NuevoAnimal { get; set; }
+        }
         // GET: Animales
         public ActionResult Index()
         {
+            AnimalesViewModel viewModel = new AnimalesViewModel();
+            viewModel.NuevoAnimal = new AnimalCLS();
+
+            using (var bd = new ESCUELAEntities())
+            {
+                viewModel.ListaMamiferos = (from Animales in bd.Animales
+                                            where Animales.TipoAnimal == "Mamiferos"
+                                            select new AnimalCLS
+                                            {
+                                                Id = Animales.ID_Animal,
+                                                Nombre = Animales.Nombre,
+                                                Descripcion = Animales.Descripcion,
+                                                ImagenUrl = Animales.Imagen
+                                            }).ToList();
+
+                viewModel.ListaAves = (from Animales in bd.Animales
+                                       where Animales.TipoAnimal == "Aves"
+                                       select new AnimalCLS
+                                       {
+                                           Id = Animales.ID_Animal,
+                                           Nombre = Animales.Nombre,
+                                           Descripcion = Animales.Descripcion,
+                                           ImagenUrl = Animales.Imagen
+                                       }).ToList();
+
+                viewModel.ListaReptiles = (from Animales in bd.Animales
+                                           where Animales.TipoAnimal == "Reptiles"
+                                           select new AnimalCLS
+                                           {
+                                               Id = Animales.ID_Animal,
+                                               Nombre = Animales.Nombre,
+                                               Descripcion = Animales.Descripcion,
+                                               ImagenUrl = Animales.Imagen
+                                           }).ToList();
+
+                viewModel.ListaAnfibios = (from Animales in bd.Animales
+                                           where Animales.TipoAnimal == "Anfibios"
+                                           select new AnimalCLS
+                                           {
+                                               Id = Animales.ID_Animal,
+                                               Nombre = Animales.Nombre,
+                                               Descripcion = Animales.Descripcion,
+                                               ImagenUrl = Animales.Imagen
+                                           }).ToList();
+
+                viewModel.ListaVertebrados = (from Animales in bd.Animales
+                                              where Animales.TipoAnimal == "Vertebrados"
+                                              select new AnimalCLS
+                                              {
+                                                  Id = Animales.ID_Animal,
+                                                  Nombre = Animales.Nombre,
+                                                  Descripcion = Animales.Descripcion,
+                                                  ImagenUrl = Animales.Imagen
+                                              }).ToList();
+
+                viewModel.ListaInvertebrados = (from Animales in bd.Animales
+                                                where Animales.TipoAnimal == "Invertebrados"
+                                                select new AnimalCLS
+                                                {
+                                                    Id = Animales.ID_Animal,
+                                                    Nombre = Animales.Nombre,
+                                                    Descripcion = Animales.Descripcion,
+                                                    ImagenUrl = Animales.Imagen
+                                                }).ToList();
+
+            }
+            return View(viewModel);
+        }
+
+        // GET: Animales/Agregar
+        public ActionResult Agregar()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Agregar(AnimalCLS nuevoAnimal, HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var bd = new ESCUELAEntities())
+                {
+                    Animales oAnimales = new Animales();
+                    oAnimales.Nombre = nuevoAnimal.Nombre;
+                    oAnimales.TipoAnimal = nuevoAnimal.TipoAnimal;
+                    oAnimales.Descripcion = nuevoAnimal.Descripcion;
+
+                    // Manejo de la imagen
+                    if (upload != null && upload.ContentLength > 0)
+                    {
+                        string fileName = Path.GetFileName(upload.FileName);
+                        string folderPath = Server.MapPath("~/Content/Imagenes");
+
+                        // Verificar si la carpeta existe, si no, crearla
+                        if (!Directory.Exists(folderPath))
+                        {
+                            Directory.CreateDirectory(folderPath);
+                        }
+
+                        string path = Path.Combine(folderPath, fileName);
+                        upload.SaveAs(path);
+                        oAnimales.Imagen = "~/Content/Imagenes/" + fileName;
+                    }
+
+
+                    bd.Animales.Add(oAnimales);
+                    bd.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+
+            // Si el modelo no es válido, vuelve a la vista con el modelo
+            var viewModel = new AnimalesViewModel { NuevoAnimal = nuevoAnimal };
+            return View("Index", viewModel);
+        }
+
+
     }
 }
--- Proyecto/Models/AnimalCLS.cs
+++ Proyecto/Models/AnimalCLS.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,24 @@ namespace Proyecto.Models
 {
     public class AnimalCLS
     {
+        [Display (Name = "Id Animal")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength (250, ErrorMessage ="La longitud maxima es 250")]
+        [Display (Name ="Nombre del Animal")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La Caracteristicas del Animal es obligatorio.")]
+        [StringLength(750, ErrorMessage = "La longitud maxima es 750")]
+        [Display(Name ="Caracteristicas Puntuales del Animal")]
         public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La Clasificacion del Animal es obligatorio.")]
+        [Display (Name ="Clasificacion del Animal")]
         public string TipoAnimal { get; set; }
+
+        [Display (Name ="Imagen del Animal")]
         public string ImagenUrl { get; set; }
     }
 }
