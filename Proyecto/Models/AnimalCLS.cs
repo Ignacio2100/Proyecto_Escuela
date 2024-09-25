41ba4995a2097b747354304cb319b202a11622fb
Ignacio2100 <ignaciodeleone8@gmail.com>
2024-09-24 22:11:57 -0600

    Cambios de ortografia

--- Proyecto/Models/AnimalCLS.cs
+++ Proyecto/Models/AnimalCLS.cs
@@ -12,17 +12,17 @@ namespace Proyecto.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
-        [StringLength (250, ErrorMessage ="La longitud maxima es 250")]
+        [StringLength (250, ErrorMessage ="La longitud máxima es 250")]
         [Display (Name ="Nombre del Animal")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "La Caracteristicas del Animal es obligatorio.")]
-        [StringLength(750, ErrorMessage = "La longitud maxima es 750")]
-        [Display(Name ="Caracteristicas Puntuales del Animal")]
+        [Required(ErrorMessage = "La Características del Animal es obligatorio.")]
+        [StringLength(750, ErrorMessage = "La longitud máxima es 750")]
+        [Display(Name ="Características Puntuales del Animal")]
         public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = "La Clasificacion del Animal es obligatorio.")]
-        [Display (Name ="Clasificacion del Animal")]
+        [Required(ErrorMessage = "La Clasificación del Animal es obligatorio.")]
+        [Display (Name ="Clasificación del Animal")]
         public string TipoAnimal { get; set; }
 
         [Display (Name ="Imagen del Animal")]
--- Proyecto/Models/FrutaCLS.cs
+++ Proyecto/Models/FrutaCLS.cs
@@ -12,20 +12,20 @@ namespace Proyecto.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
-        [StringLength(250, ErrorMessage = "La longitud maxima es 250")]
-        [Display(Name = "Nombre del Elemento")]
+        [StringLength(250, ErrorMessage = "La longitud máxima es 250")]
+        [Display(Name = "Nombre del elemento")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "La Caracteristicas del Elemento es obligatorio.")]
-        [StringLength(750, ErrorMessage = "La longitud maxima es 750")]
-        [Display(Name = "Caracteristicas Puntuales del Elemento")]
+        [Required(ErrorMessage = "La características del elemento es obligatorio.")]
+        [StringLength(750, ErrorMessage = "La longitud máxima es 750")]
+        [Display(Name = "Características Puntuales del Elemento")]
         public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = "La Clasificacion del Elemento es obligatorio.")]
-        [Display(Name = "Clasificacion del Elemento")]
+        [Required(ErrorMessage = "La clasificación del elemento es obligatorio.")]
+        [Display(Name = "Clasificación del elemento")]
         public string TipoElemento{ get; set; }
 
-        [Display(Name = "Imagen del Elemento")]
+        [Display(Name = "Imagen del elemento")]
         public string ImagenUrl { get; set; }
     }
 }
